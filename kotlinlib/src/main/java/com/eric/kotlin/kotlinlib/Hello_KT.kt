f395aa2f5c965d928682add0aa73453104b44d7a
Eric <yuanshuaiding@163.com>
2018-11-22 16:49:08 +0800

    Initial commit

---
+++ kotlinlib/src/main/java/com/eric/kotlin/kotlinlib/Hello_KT.kt
@@ -0,0 +1,14 @@
+package com.eric.kotlin.kotlinlib
+
+/**
+ * author : Eric
+ * e-mail : yuanshuai@bertadata.com
+ * time   : 2018/11/22
+ * desc   : hello world for kotlin
+ * version: 1.0
+ */
+
+//kotlin 1.3之后main入口方法可以省略参数,666
+fun main(){
+    println("hello world from kotlin")
+}
